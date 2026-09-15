e28ad95938669f083e6aa7ab664245702aea73d2
RUSHABHAVHAD <rushabhavhad96@gmail.com>
2026-09-15 21:55:54 +0530

    Add player initialization and save functions
    Initialize player data in local storage if not present.

--- js/storage.js
+++ js/storage.js
@@ -1,1 +1,38 @@
+const STORAGE_KEY = "ariseFitnessPlayer";
 
+let player = JSON.parse(
+  localStorage.getItem(STORAGE_KEY)
+);
+
+if (!player) {
+  player = {
+    level: 1,
+    xp: 0,
+    totalXp: 0,
+    workouts: 0,
+    streak: 0,
+    completedExercises: [],
+    history: []
+  };
+}
+
+function savePlayer() {
+  localStorage.setItem(
+    STORAGE_KEY,
+    JSON.stringify(player)
+  );
+}
+
+function resetPlayer() {
+  player = {
+    level: 1,
+    xp: 0,
+    totalXp: 0,
+    workouts: 0,
+    streak: 0,
+    completedExercises: [],
+    history: []
+  };
+
+  savePlayer();
+}
